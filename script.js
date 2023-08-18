6ddc6a04b420235586e0c22c73a85c9bf0f06123
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-08-18 11:47:59 +0500

    files and folders created

---
+++ script.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAvpH542wCxa_VjokuMWrVBNpkWA1vgUo",
+  authDomain: "boiler-plate-6ea86.firebaseapp.com",
+  projectId: "boiler-plate-6ea86",
+  storageBucket: "boiler-plate-6ea86.appspot.com",
+  messagingSenderId: "859017546758",
+  appId: "1:859017546758:web:a3639fa7975b81bee70787",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
